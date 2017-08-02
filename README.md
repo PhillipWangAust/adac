3bcb552e6d99ee40d4e97561b59beb91672f7566
Zac <zacdblanco@gmail.com>
2017-08-01 23:37:06 -0400

    Add experiment runner file

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# ADAC
+# ADAC (Aresty Distributed Average Consensus)
 
-This is an implementation of distributed average consensus which is meant to run on a variety of platforms.
+This project was supported by the Aresty Center for Undergraduate Research at Rutgers University during the spring 2016-17 AY
+
+Working with Dr. Waheed Bajwa, we created an implementation of Distributed average consensus.
+
+This respository houses the code we built to test and experiment with consensus using MPI vs a pure-TCP approach.
+
+
--- experiment.py
+++ experiment.py
@@ -1,28 +1,74 @@
+#! /usr/bin/env python3
+import json
+import requests
+import sys
+import time
 import adac.util as u
-import threading
+
 username = 'pi'
-data_loc = '/home/pi/adac/vec.txt'
+data_loc = '/home/pi/adac/data.txt'
 hosts = ['192.168.2.177',
-	 '192.168.2.178',
-	 '192.168.2.180',
-	 '192.168.2.181',
-	 '192.168.2.182', 
+         '192.168.2.178',
+         '192.168.2.180',
+         '192.168.2.181',
+         '192.168.2.182',
          '192.168.2.183',
          '192.168.2.184']
 
-def cpy_id(usr, hst1, hst2):
-	u.run(usr, hst1, ['ssh-copy-id {}@{}'.format(usr, hst2)])
+args = [ 'test', 'start', 'kill', 'restart', 'code', 'params', 'vector', 'wait']
 
-thd = []
-for host in hosts:
-	for hostx in hosts:
-#		t = threading.Thread(target=cpy_id, args=(username, host, hostx))
-#		t.start()
-		u.run(username, host, ['ssh-copy-id {}@{}'.format(username, hostx)])
+def kill_sessions():
+    '''Kill the any tmux sessions running consensus server'''
+    for host in hosts:
+        u.run(username, host, ["tmux kill-session -t run_session"])
 
-#for thred in thd:
-#	thred.join()
+def start_sessions():        
+    '''Start tmux sessions to run servers'''
+    for host in hosts:
+        u.run(username, host, ["cd ~/adac/; tmux new-session -d -s run_session 'python3 -m adac'"])
+        
+def restart_sessions():
+    '''Restarts the tmux sessions'''
+    kill_sessions()
+    start_sessions()
 
-#print(u.generate_data(username, hosts, data_loc, 25))
-#print(u.generate_data('pi', ['raspberrypi', 'daredevil'], '/home/pi/vec.txt', 10))
 
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("At least one argument required\n\n Try one of: {}".format(args))    
+    for arg in sys.argv[1:]:
+        if arg not in args:
+            print("Command {} not recognized. Try one of {}".format(sys.argv[1], args))
+        elif arg == 'test':
+            print("Testing connection to all hosts")
+            for host in hosts:
+                try:
+                    print('{} from {}'.format( requests.get('http://{}:9090/'.format(host)).status_code, host))
+                except:
+                    print("Error connecting to {}".format(host))
+        elif arg == 'restart':
+            print("Restarting all sessions")
+            restart_sessions()
+        elif arg == 'wait':
+            time.sleep(5)
+        elif arg == 'start':
+            print("Starting all host servers")
+            start_sessions()
+        elif arg == 'kill':
+            print("Killing all sessions")
+            kill_sessions()
+        elif arg == 'code':
+            print("Updating code repository")
+            for host in hosts:
+                u.distcp(username, host, "adac/", "/home/pi/adac/")
+        elif arg == 'params':
+            print("Updating params files")
+            for host in hosts:
+                u.distcp(username, host, "params.conf", "/home/pi/adac/params.conf")
+        elif arg == 'vector':
+            avg_vec, vecs = (u.generate_data(username, hosts, data_loc, 50))
+            with open('avg_data.txt', 'w') as f:
+                f.write(str(avg_vec))
+                f.write("\n")
+                f.write(json.dumps(vecs))
